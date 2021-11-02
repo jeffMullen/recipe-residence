c7a217c8b3e8d896a8f8ca482b5f8add56a4089b
Walrus <81767572+jeffMullen@users.noreply.github.com>
2021-11-02 19:42:44 -0400

    setting up skeleton files for home, navbar, and scss

---
+++ client/src/pages/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Home() {
+
+
+    return (
+        <>
+            <div className="container">
+                <div className="row">
+                    <div className="hero col"></div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Home;
