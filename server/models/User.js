f1bc8167cb88e82b0a93e171b2a82bcbd8cb1281
Rachel <rtc145@gmail.com>
2021-11-02 19:17:40 -0400

    Added User model

---
+++ server/models/User.js
@@ -0,0 +1,57 @@
+const { Schema, model } = require('mongoose');
+const bcrypt = require('bcrypt');
+
+// import schema from Recipe.js
+const recipeSchema = require('./Book');
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/.+@.+\..+/, 'Must use a valid email address'],
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    // set savedRecipes to be an array of data that adheres to the recipeSchema
+    savedRecipes: [recipeSchema],
+  },
+  // set this to use virtual below
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
+
+// hash user password
+userSchema.pre('save', async function (next) {
+  if (this.isNew || this.isModified('password')) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+
+  next();
+});
+
+// custom method to compare and validate password for logging in
+userSchema.methods.isCorrectPassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
+
+// when we query a user, we'll also get another field called `recipeCount` with the number of saved recipes we have
+userSchema.virtual('recipeCount').get(function () {
+  return this.savedRecipes.length;
+});
+
+const User = model('User', userSchema);
+
+module.exports = User;
--- server/models/index.js
+++ server/models/index.js
@@ -1,3 +1,4 @@
 // const User = require('./User');
 
 // module.exports = { User };
+
