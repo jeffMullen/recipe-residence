e4289ea96cd1c084f65fa271bbca76f86786fa4c
Walrus <81767572+jeffMullen@users.noreply.github.com>
2021-11-02 21:25:42 -0400

    creating Profile page skeleton

---
+++ client/src/pages/Profile.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Profile() {
+
+    return (
+        <>
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="card">
+                        <h2>Recipe</h2>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+
+}
+
+export default Profile;
