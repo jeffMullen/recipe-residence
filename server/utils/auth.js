d0cc56dc1dce90a9ab67ea306316fee6743152db
Rachel <rtc145@gmail.com>
2021-11-02 20:53:37 -0400

    Added user authentication logic to server/utils/auth.js

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -3,4 +3,35 @@ const jwt = require('jsonwebtoken');
 const secret = process.env.DB_SECRET;
 const expiration = '2h';
 
-module.exports = {};
+module.exports = {
+    // function for our authenticated routes
+  authMiddleware: function ({req}) {
+    // allows token to be sent via  req.query or headers
+    let token = req.query.token || req.headers.authorization;
+
+    if (req.headers.authorization) {
+      token = token.split(' ').pop().trim();
+    }
+
+    if (!token) {
+      return req;
+    }
+
+    // verify token and get user data out of it
+    try {
+      const { data } = jwt.verify(token, secret, { maxAge: expiration });
+      req.user = data;
+    } catch {
+      console.log('Invalid token');
+    }
+
+    // send to next endpoint
+    return req;
+  },
+  signToken: function ({ username, email, _id }) {
+    const payload = { username, email, _id };
+
+    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+  },
+};
+
