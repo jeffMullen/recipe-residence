e3ff17fdd7cd5bed7d04c8865df7e2d9461c4515
Walrus <81767572+jeffMullen@users.noreply.github.com>
2021-11-03 13:15:43 -0400

    recipe card populated with dummy data

--- client/src/components/RecipeCard.js
+++ client/src/components/RecipeCard.js
@@ -1,13 +1,26 @@
 import React from 'react';
 
-function RecipeCard() {
+function RecipeCard({ recipe }) {
+
+    const { title, ingredients, description, instructions, total_time, link } = recipe;
 
 
     return (
         <>
-            
+            <div classNameName="col">
+                <div className="card">
+                    <img src="..." className="card-img-top" alt="..."></img>
+                    <div className="card-body">
+                        <h5 className="card-title">{title}</h5>
+                        <p>{total_time}</p>
+                        <p>{description}</p>
+                        <ul className="card-text">{ingredients.map(ingredient => <li>{ingredient}</li>)}</ul>
+                        <p className="card-text">{instructions}</p>
+                        <a href={link} rel="noreferrer" target="_blank">Blog Recipe</a>
+                    </div>
+                </div>
+            </div>
         </>
-
     )
 }
 
--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import RecipeCard from "../components/RecipeCard";
 
 function Profile() {
 
+    let [recipes, setRecipes] = useState([]);
+
+    recipes = [
+        {
+            title: 'Lasagna',
+            ingredients: ['Noodles', 'Mozz', 'Sauce', 'Eggplant'],
+            description: 'Super good Italian flat noodle pie',
+            instructions: 'Boil noodles, put in casserole dish, add sauce and such',
+            total_time: '1hr 30mins',
+            link: 'lasagna.com'
+        },
+        {
+            title: 'Lasagna',
+            ingredients: ['Noodles', 'Mozz', 'Sauce', 'Eggplant'],
+            description: 'Super good Italian flat noodle pie',
+            instructions: 'Boil noodles, put in casserole dish, add sauce and such',
+            total_time: '1hr 30mins',
+            link: 'lasagna.com'
+        }
+    ]
+
     return (
         <>
-            <RecipeCard />
+            {recipes.map(recipe => <RecipeCard recipe={recipe} />)}
         </>
     )
 
