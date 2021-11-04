9e5589e94d81cc9d23e2d82f11bf78e85a2a6c99
Rachel <rtc145@gmail.com>
2021-11-04 19:41:27 -0400

    Added login/signup frontend and connected to backend

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React , {useState} from 'react';
 import Navbar from 'react-bootstrap/Navbar';
-
+import { Link } from 'react-router-dom';
+import { Modal, Tab} from 'react-bootstrap';
 import Nav from 'react-bootstrap/Nav';
 import Container from 'react-bootstrap/Container';
+import Auth from '../utils/auth';
+import SignUpForm from './SignupForm';
+import LoginForm from './LoginForm';
 
 function Navigation() {
-
+    // set modal display state
+    const [showModal, setShowModal] = useState(false);
 
     return (
         <>
@@ -14,9 +19,24 @@ function Navigation() {
                     <Navbar.Brand>Recipe Repo</Navbar.Brand>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end">
-                        <Nav className="me-auto d-lg-flex m-lg-0">
-                            <Nav.Link href="/login">Login</Nav.Link>
+                        <Nav className='ml-auto'>
+                            {/* if user is logged in show saved books and logout */}
+                            {Auth.loggedIn() ? (
+                                <>
+                                    <Nav.Link as={Link} to='/saved'>
+                                        See Your Recipes
+                                    </Nav.Link>
+                                    <Nav.Link onClick={Auth.logout}>Logout</Nav.Link>
+                                </>
+                            ) : (
+                                <Nav.Link onClick={() => setShowModal(true)}>Login/Sign Up</Nav.Link>
+                            )}
                         </Nav>
+
+                        {/* <Nav className="me-auto d-lg-flex m-lg-0">
+                            <Nav.Link href="/login">Login/Signup</Nav.Link>
+                        </Nav> */}
+
                         <form className="d-flex col-12 col-lg-6">
                             <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"
                                 id="dietary-restriction-search"></input>
@@ -25,31 +45,37 @@ function Navigation() {
                     </Navbar.Collapse>
                 </Container>
             </Navbar>
-            {/* <nav className="navbar-app navbar navbar-expand-lg py-4 d-flex align-items-center">
-                <h1 className="px-3 m-0 d-flex"><a className="navbar-brand" id="site-title" href="/">Recipe Repo</a></h1>
-                <button className="navbar-toggler navbar-light" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                    aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse p-0 mr-0 ml-3 mb-3 mb-lg-0 col-lg-7 col-xl-5"
-                    id="navbarSupportedContent">
-                    <form className="d-flex col-12 col-lg-6">
-                        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"
-                            id="dietary-restriction-search"></input>
-                        <button id="search" className="btn btn-outline-success" type="submit">Search</button>
-                    </form>
-                    <ul
-                        className="row navbar-nav">
-                        <li className="nav-item pl-0">
-                            <a id="logout" className="nav-link active mr-2" aria-current="page" href="#">Logout</a>
-                        </li>
-                        <li class="nav-item pl-0">
-                            <a id="login" class="nav-link active mr-2" aria-current="page" href="#">Login</a>
-                        </li>
-                    </ul>
-                </div>
-            </nav> */}
+            <Modal
+                size='lg'
+                show={showModal}
+                onHide={() => setShowModal(false)}
+                aria-labelledby='signup-modal'>
+                {/* tab container to do either signup or login component */}
+                <Tab.Container defaultActiveKey='login'>
+                    <Modal.Header closeButton>
+                        <Modal.Title id='signup-modal'>
+                            <Nav variant='pills'>
+                                <Nav.Item>
+                                    <Nav.Link eventKey='login'>Login</Nav.Link>
+                                </Nav.Item>
+                                <Nav.Item>
+                                    <Nav.Link eventKey='signup'>Sign Up</Nav.Link>
+                                </Nav.Item>
+                            </Nav>
+                        </Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <Tab.Content>
+                            <Tab.Pane eventKey='login'>
+                                <LoginForm handleModalClose={() => setShowModal(false)} />
+                            </Tab.Pane>
+                            <Tab.Pane eventKey='signup'>
+                                <SignUpForm handleModalClose={() => setShowModal(false)} />
+                            </Tab.Pane>
+                        </Tab.Content>
+                    </Modal.Body>
+                </Tab.Container>
+            </Modal>
         </>
     )
 }
