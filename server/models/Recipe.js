ad36ca0b84b3a1363f5432630866f1e30cdfec7f
Zachary Zaleski <83981829+kingcrowz@users.noreply.github.com>
2021-11-04 19:15:25 -0400

    Update Recipe.js
    added ID and author to the schema for recipe

--- server/models/Recipe.js
+++ server/models/Recipe.js
@@ -2,6 +2,16 @@ const { Schema, model } = require('mongoose');
 
 // This is a subdocument schema, it won't become its own model but we'll use it as the schema for the User's `savedRecipes` array in User.js
 const recipeSchema = new Schema({
+    _id: {
+        type: ID,
+        required: true,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    author: {
+        type: String,
+        required: true,
+    },
     title: {
         type: String,
         required: true,
