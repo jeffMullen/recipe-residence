8a1aa66aa6f43027c732c226551a0cc092ce6627
lapshaffer <lapshaffer@gmail.com>
2021-11-04 18:52:55 -0400

    tentative update to queries.js, pending additional recipe keys

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -2,41 +2,67 @@
 
 import { gql } from '@apollo/client';
 
-// add recipes object
 export const GET_USER = gql`
 query user($username: String!) {
   user(username: $username) {
     _id
     username
     email
-    
+    recipes {
+      _id
+      title
+      ingredients
+      description
+      instructions
+      total_time
+      dietry_restrictions
+    }
   }
 }
 `;
 
-// add recipes object
 export const GET_ME = gql`
   query me {
     me {
       _id
       username
       email
-  
+      savedRecipes {
+      _id
+      title
+      ingredients
+      description
+      instructions
+      total_time
+      dietry_restrictions
+      }
     }
   }
 `;
 
-// add recipes object
 export const GET_RECIPES = gql`
   query recipes {
-    
+    recipes {
+      _id
+      title
+      ingredients
+      description
+      instructions
+      total_time
+      dietry_restrictions
+    }
   }
 `;
 
-// add recipe object
 export const GET_SINGLE_RECIPE = gql`
   query recipe($recipeId: ID!) {
     recipe(recipeId: $recipeId) {
-    
+    _id
+    title
+    ingredients
+    description
+    instructions
+    total_time
+    dietry_restrictions
   }
 `;
