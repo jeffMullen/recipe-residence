de6427d39c1f4643742657c68c3a241fd56b7949
lapshaffer <lapshaffer@gmail.com>
2021-11-04 18:38:34 -0400

    updating user queries

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,3 +1,42 @@
 // Any actions that fetch data to display on the page
 
-// import { gql } from '@apollo/client';
+import { gql } from '@apollo/client';
+
+// add recipes object
+export const GET_USER = gql`
+query user($username: String!) {
+  user(username: $username) {
+    _id
+    username
+    email
+    
+  }
+}
+`;
+
+// add recipes object
+export const GET_ME = gql`
+  query me {
+    me {
+      _id
+      username
+      email
+  
+    }
+  }
+`;
+
+// add recipes object
+export const GET_RECIPES = gql`
+  query recipes {
+    
+  }
+`;
+
+// add recipe object
+export const GET_SINGLE_RECIPE = gql`
+  query recipe($recipeId: ID!) {
+    recipe(recipeId: $recipeId) {
+    
+  }
+`;
