b966dae1c9d161b0a05fe84b662828a9f430478f
Walrus <81767572+jeffMullen@users.noreply.github.com>
2021-11-04 21:07:27 -0400

    defining height for recipe cards

--- client/src/components/RecipeCard.js
+++ client/src/components/RecipeCard.js
@@ -11,21 +11,23 @@ function RecipeCard({ recipe }) {
 
     return (
         <>
-            <Link
-                className={`${styles.recipeLink} col col-sm-6 col-md-4 col-lg-3`}
-                to={`/recipes/${link}`}>
-                <div className="card">
-                    <img src={recipeImg} className="card-img-top" alt="..."></img>
-                    <div className="card-body">
-                        <h5 className="card-title">{title}</h5>
-                        <p>{total_time}</p>
-                        <p>{description}</p>
-                        <ul className="card-text">{ingredients.map(ingredient => <li>{ingredient}</li>)}</ul>
-                        <p className="card-text">{instructions}</p>
-                        <a href={link} rel="noreferrer" target="_blank">Blog Recipe</a>
+            <div className={`col-12 col-sm-6 col-md-4 col-lg-3`}>
+                <Link
+                    className={`${styles.recipeLink}`}
+                    to={`/recipes/${link}`}>
+                    <div className={`${styles.recipeCard} card`}>
+                        <img src={recipeImg} className="card-img-top" alt="..."></img>
+                        <div className={`${styles.cardBody} card-body`}>
+                            <h5 className="card-title">{title}</h5>
+                            <p>{total_time}</p>
+                            <p>{description}</p>
+                            <ul className="card-text">{ingredients.map(ingredient => <li>{ingredient}</li>)}</ul>
+                            <p className="card-text">{instructions}</p>
+                            <a href={link} rel="noreferrer" target="_blank">Blog Recipe</a>
+                        </div>
                     </div>
-                </div>
-            </Link>
+                </Link>
+            </div>
         </>
     )
 }
