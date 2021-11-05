8a4f0306c2db01bef5113bb1d34248141cd6ecfb
Rachel <rtc145@gmail.com>
2021-11-04 20:32:07 -0400

    updated typedefs and mutations and got server working

--- server/models/Recipe.js
+++ server/models/Recipe.js
@@ -2,12 +2,12 @@ const { Schema, model } = require('mongoose');
 
 // This is a subdocument schema, it won't become its own model but we'll use it as the schema for the User's `savedRecipes` array in User.js
 const recipeSchema = new Schema({
-    _id: {
-        type: ID,
-        required: true,
-        primaryKey: true,
-        autoIncrement: true,
-    },
+    // _id: {
+    //     type: ID,
+    //     required: true,
+    //     primaryKey: true,
+    //     autoIncrement: true,
+    // },
     author: {
         type: String,
         required: true,
@@ -31,7 +31,7 @@ const recipeSchema = new Schema({
     total_time: {
         type: String
     },
-    dietry_restrictions: [
+    dietary_restrictions: [
         {
           type: Schema.Types.ObjectId,
           ref: 'DietaryRestrictions'
