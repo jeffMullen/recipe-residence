dbd2e71a1a1c18c3e35cf91aeddebfe13b88fb3e
Walrus <81767572+jeffMullen@users.noreply.github.com>
2021-11-06 13:31:19 -0400

    unfinished checkbox validation

--- client/src/components/CreateRecipe/RecipeForm/Checkbox.js
+++ client/src/components/CreateRecipe/RecipeForm/Checkbox.js
@@ -5,7 +5,9 @@ function Checkbox({ restriction }) {
     return (
         <>
             <div className="form-check">
-                <input className="form-check-input" type="checkbox" value={restriction} id="flexCheckDefault"></input>
+                <input
+                    onClick={(e) => console.log(e.target.checked)}
+                    className="form-check-input" type="checkbox" value={restriction} id="flexCheckDefault"></input>
                 <label className="form-check-label" htmlFor="flexCheckDefault">
                     {restriction}
                 </label>
