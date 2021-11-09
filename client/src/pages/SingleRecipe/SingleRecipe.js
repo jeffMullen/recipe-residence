931926f1e2ab2230393591dc8afa52be4ae3c770
Walrus <81767572+jeffMullen@users.noreply.github.com>
2021-11-08 21:48:07 -0500

    can delete dietary_restrictions, ingredients, and instructions from state arrays; can change title, totalTime, and description in state

--- client/src/pages/SingleRecipe/SingleRecipe.js
+++ client/src/pages/SingleRecipe/SingleRecipe.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import { useParams } from 'react-router-dom';
@@ -14,6 +14,73 @@ const SingleRecipe = () => {
     const { loading, data } = useQuery(GET_SINGLE_RECIPE, {
         variables: { recipeId },
     });
+
+    const recipe = data?.getSingleRecipe || {};
+    console.log(recipe);
+
+    const { author, description, dietary_restrictions, total_time, title, instructions, ingredients } = recipe;
+
+    const [recipeTitle, setRecipeTitle] = useState(title);
+    const [totalTime, setTotalTime] = useState(total_time);
+    const [recipeDescription, setRecipeDescription] = useState(description);
+    const [recipeInstructions, setRecipeInstructions] = useState(instructions);
+    const [recipeIngredients, setRecipeIngredients] = useState(ingredients);
+    const [restrictions, setRestrictions] = useState(dietary_restrictions);
+
+    const handleDelete = (e, item) => {
+        e.preventDefault();
+        let name = item.getAttribute('name');
+        let value = item.textContent;
+
+        if (name === 'dietaryRestrictions') {
+            let index = restrictions.indexOf(value);
+
+            if (index > -1) {
+                let newRestrictions = restrictions.filter(restriction => {
+                    return restriction !== value;
+                });
+                setRestrictions(newRestrictions);
+            }
+        }
+        if (name === 'ingredients') {
+            let index = recipeIngredients.indexOf(value);
+
+            if (index > -1) {
+                let newIngredients = recipeIngredients.filter(ingredient => {
+                    return ingredient !== value;
+                });
+                setRecipeIngredients(newIngredients);
+            }
+        }
+        if (name === 'instructions') {
+            let index = recipeInstructions.indexOf(value);
+
+            if (index > -1) {
+                let newInstructions = recipeInstructions.filter(instruction => {
+                    return instruction !== value;
+                });
+                setRecipeInstructions(newInstructions);
+            }
+        }
+    }
+
+    const handleInputChange = (e, input) => {
+        e.preventDefault();
+        let name = input.name;
+        let value = input.value;
+        console.log(name, value)
+
+        if (name === 'title') {
+            setRecipeTitle(value);
+        }
+        if (name === 'totalTime') {
+            setTotalTime(value);
+        }
+        if (name === 'description') {
+            setRecipeDescription(value);
+        }
+    }
+
     const { loading: getMeLoading, data: getMeData } = useQuery(GET_ME);
     console.log(getMeData);
 
@@ -23,11 +90,10 @@ const SingleRecipe = () => {
 
     let savedRecipes = getMeData?.me?.saved_recipes;
     console.log(savedRecipes);
-    // If logged in && profile._id === recipe.userId {
-    // const admin = true;
 
     let admin = false;
 
+    // If logged in and current recipe ID matches a recipe ID in user saved_recipes: set admin priviledges to true
     if (Auth.loggedIn()) {
         const profile = Auth.getProfile().data._id;
         console.log('profile', profile)
@@ -43,10 +109,6 @@ const SingleRecipe = () => {
 
     Auth.loggedIn() && admin ? console.log('ADMIN') : console.log('NO ACCESS');
 
-    const recipe = data?.getSingleRecipe || {};
-    console.log(recipe);
-
-    const { author, description, dietary_restrictions, total_time, title, instructions, ingredients } = recipe;
 
     if (loading) {
         return <div>Loading...</div>;
@@ -55,42 +117,125 @@ const SingleRecipe = () => {
         <>
             {Auth.loggedIn() && admin ?
                 // If logged in and recipe is authored by user, render a recipe that can be updated
-                <div className={`${styles.recipe} my-3 mx-3`}>
-                    <h2>
-                        {title}
-                    </h2>
+                <div className={`${styles.recipe} my-3 mx-3 p-5`}>
                     <div>
-                        <p>{total_time}</p>
+                        <h2>
+                            {recipeTitle}
+                        </h2>
+                        <div>
+                            <input id="title" name="title" aria-describedby=""></input>
+                            <button onClick={(e) => {
+                                handleInputChange(e, e.target.previousSibling)
+                                e.target.previousSibling.value = '';
+                            }}>
+                                Change
+                            </button>
+                        </div>
+                    </div>
+                    <div>
+                        <p>{totalTime}</p>
+                        <div>
+                            <input id="totalTime" name="totalTime" aria-describedby=""></input>
+                            <button onClick={(e) => {
+                                handleInputChange(e, e.target.previousSibling)
+                                e.target.previousSibling.value = '';
+                            }}>
+                                Change
+                            </button>
+                        </div>
                     </div>
                     <Link to={`/profiles/${author}`}>
                         {author}
                     </Link>
                     <div>
-                        <p>{description}</p>
+                        <p>{recipeDescription}</p>
+                        <div>
+                            <input id="description" name="description" aria-describedby=""></input>
+                            <button onClick={(e) => {
+                                handleInputChange(e, e.target.previousSibling)
+                                e.target.previousSibling.value = '';
+                            }}>
+                                Change
+                            </button>
+                        </div>
                     </div>
-                    <div>
-                        <h3>Dietary Restrictions</h3>
-                        <ul>
-                            {dietary_restrictions.map((restriction, index) =>
-                                <div key={index}>
-                                    <li>{restriction}</li>
-                                    <button onClick={() => console.log("CLICK")}>
-                                        X
-                                    </button>
-                                </div>
-                            )}
-                        </ul>
-                    </div>
-                    <div>
-                        <ul>
-                            {ingredients.map((ingredient, index) => <li key={index}>{ingredient}</li>)}
-                        </ul>
-                    </div>
-                    <div>
-                        <ol>
-                            {instructions.map((instruction, index) => <li key={index}>{instruction}</li>)}
-                        </ol>
-                    </div>
+                    {restrictions ?
+                        <div>
+                            <h3>Dietary Restrictions</h3>
+                            <ul className={styles.dietaryRestrictions}>
+                                {restrictions.map((restriction, index) =>
+                                    <div key={index}
+                                        className={`${styles.listItemContainer}`}>
+                                        <li className={styles.listItem} name="dietaryRestrictions" value={restriction}>{restriction}</li>
+                                        <button
+                                            className={styles.deleteButton}
+                                            onClick={(e) => {
+
+                                                handleDelete(e, e.target.previousSibling)
+                                            }}>
+                                            X
+                                        </button>
+                                    </div>
+                                )}
+                            </ul>
+                        </div>
+
+                        :
+
+                        <div></div>
+                    }
+                    {recipeIngredients ?
+                        <div>
+                            <h3>Ingredients</h3>
+                            <ul>
+                                {recipeIngredients.map((ingredient, index) =>
+                                    <div key={index}
+                                        className={styles.listItemContainer}>
+                                        <li className={styles.listItem} name="ingredients" value={ingredient}>{ingredient}</li>
+                                        <button
+                                            className={styles.deleteButton}
+                                            onClick={(e) => {
+
+                                                handleDelete(e, e.target.previousSibling)
+                                            }}>
+                                            X
+                                        </button>
+                                    </div>
+                                )}
+                            </ul>
+                        </div>
+
+                        :
+
+                        <div></div>
+
+                    }
+                    {recipeInstructions ?
+                        <div>
+                            <h3>Instructions</h3>
+                            <ol>
+                                {recipeInstructions.map((instruction, index) =>
+                                    <div key={index}
+                                        className={styles.listItemContainer}>
+                                        <li className={styles.listItem} name="instructions" value={instruction}>{instruction}</li>
+                                        <button
+                                            className={styles.deleteButton}
+                                            onClick={(e) => {
+
+                                                handleDelete(e, e.target.previousSibling)
+                                            }}>
+                                            X
+                                        </button>
+                                    </div>
+                                )}
+                            </ol>
+                        </div>
+
+                        :
+
+                        <div></div>
+
+                    }
                 </div>
 
                 :
